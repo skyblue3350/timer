dc5101d7eaa8c8ac1dedafee1d2552d960e4454c
sky <skyblue3350@gmail.com>
2020-04-05 02:49:17 +0900

    デザイン大方決まり

--- src/renderer/app.tsx
+++ src/renderer/app.tsx
@@ -1,26 +1,88 @@
 import * as React from 'react';
-import { Message, Icon } from 'semantic-ui-react'
+import { Button, Grid, Progress, Table } from 'semantic-ui-react';
 
 export interface Props {
 }
 
 export interface State {
+    presets: number[]
+    time: number
 }
 
 export default class App extends React.Component<Props, State> {
+    timer: void
+
     constructor(props: Props) {
         super(props)
+
+        this.state = {
+            presets: [],
+            time: 0,
+        }
     }
 
     render(): JSX.Element {
         return (
-            <Message icon>
-                <Icon name='circle notched' loading />
-                <Message.Content>
-                    <Message.Header>Sample Option</Message.Header>
-                    Sample Message
-                </Message.Content>
-            </Message>
+            <Grid padded={false}>
+                <Grid.Row columns={1} centered>
+                    <Progress percent={100} color='green' size='tiny' attached='bottom'/>
+                    <Button.Group widths={3}>
+                        <Button content='+'/>
+                        <Button content='+'/>
+                        <Button content='+'/>
+                    </Button.Group>
+                </Grid.Row>
+
+                <Grid.Row columns={3}>
+                    {['hour', 'min', 'sec'].map((className) => {
+                        return <Grid.Column
+                            key={className}
+                            className={className}
+                            textAlign='center'
+                            verticalAlign='middle'
+                            style={{fontSize: '25vh', lineHeight: '1em'}}
+                            onKeyPress={(e: Event) => console.log(e)}>
+                            00
+                        </Grid.Column>
+                    })}
+                </Grid.Row>
+
+                <Grid.Row columns={1} centered>
+                    <Button.Group widths={3}>
+                        <Button content='-'/>
+                        <Button content='-'/>
+                        <Button content='-'/>
+                    </Button.Group>
+                </Grid.Row>
+
+                <Grid.Row columns={1} centered>
+                    <Button.Group widths={3}>
+                        <Button content='Start' color='green' />
+                        <Button content='Stop' color='red' />
+                        <Button content='Add Preset' color='blue'/>
+                    </Button.Group>
+                </Grid.Row>
+
+                <Grid.Row>
+                    <Grid.Column>
+                        <Table striped unstackable>
+                            <Table.Body>
+                                {this.state.presets.map((preset: number) => {
+                                <Table.Row>
+                                    <Table.Cell>
+                                        {preset}
+                                    </Table.Cell>
+                                    <Table.Cell textAlign='right'>
+                                        <Button color='blue' circular icon={{name: 'add'}} />
+                                        <Button color='red' circular icon={{name: 'minus'}} />
+                                    </Table.Cell>
+                                </Table.Row>
+                                })}
+                            </Table.Body>
+                        </Table>
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
         )
     }
 }
